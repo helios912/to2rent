8b94c2d0f9a1f2fab7fbb965913a546d34d05b97
Mukola2 <janaon333@gmail.com>
2024-03-19 14:34:54 +0200

    2part

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-
+$(function () {
+  $('.slider').slick({
+    nextArrow:
+      '<button class="slick-arrow slick-next"><img src="../images/arrow-right.svg" alt=""></button>',
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><img src="../images/arrow-left.svg" alt=""></button>',
+  });
 });
