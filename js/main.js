2704f5b8e949382d9494964746131f161a1ac354
Mikola <das19901990a@gmail.com>
2024-03-20 13:01:28 +0200

    qwer

--- js/main.js
+++ js/main.js
@@ -5,4 +5,14 @@ $(function () {
     prevArrow:
       '<button class="slick-arrow slick-prev"><img src="../images/arrow-left.svg" alt=""></button>',
   });
+  $('.slider2').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+
+    nextArrow:
+      '<button class="slick-arrow slick-next"><img src="../images/arrow-right.svg" alt=""></button>',
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><img src="../images/arrow-left.svg" alt=""></button>',
+  });
 });
