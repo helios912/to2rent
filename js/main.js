407c2dbd6cf1603c7cd0eb37649413332b893625
Mikola <das19901990a@gmail.com>
2024-03-20 19:37:16 +0200

    end

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ $(function () {
       '<button class="slick-arrow slick-next"><img src="../images/arrow-right.svg" alt=""></button>',
     prevArrow:
       '<button class="slick-arrow slick-prev"><img src="../images/arrow-left.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 441,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
   $('.slider2').slick({
     arrows: false,
@@ -14,5 +22,11 @@ $(function () {
       '<button class="slick-arrow slick-next"><img src="../images/arrow-right.svg" alt=""></button>',
     prevArrow:
       '<button class="slick-arrow slick-prev"><img src="../images/arrow-left.svg" alt=""></button>',
+  });
+  $('.burger__menu').on('click', function () {
+    $('.menu').addClass('active');
+  });
+  $('.close-btn').on('click', function () {
+    $('.menu').removeClass('active');
   });
 });
